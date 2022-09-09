b4dd258d843446abbf5ccc9159928421f9b4b4b4
EzequielSchepke22028 <ezequiel.schepke@gmail.com>
2022-09-09 16:22:35 -0300

    firts commit

--- JavaAvanzado-22813-cac-master/src/asociaciones/CursoTest.java
+++ JavaAvanzado-22813-cac-master/src/asociaciones/CursoTest.java
@@ -1,6 +1,7 @@
 package asociaciones;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
